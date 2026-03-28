0e30c489ccae1632b0a7b0d4502dc50d27d30513
vc <vc@vcs-MacBook-Air.local>
2026-03-28 15:44:59 +0530

    updated-cross-issue

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,16 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //Enable global validation
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "https://api.abroadscholars.in",
+      "https://crm.abroadscholars.in",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,7 +23,7 @@ async function bootstrap() {
     }),
   );
 
-  // Swagger Configuration.
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle("Aboard Scholar CRM API")
     .setDescription("CRM backend API documentation")
@@ -24,6 +33,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
+
   await app.listen(5000, "0.0.0.0");
 }
 bootstrap();
